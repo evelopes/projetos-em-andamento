0860ba82bde697f88aea1057af5aced6940ecbab
evelopes <evelinheks@gmail.com>
2022-03-13 21:07:48 -0300

    Tabuada, node

--- tabuada.js
+++ tabuada.js
@@ -1,11 +1,11 @@
 // 06/03/2022
-let operacao = "multiplicacao"; // ALTERAR PARA: soma, subtracao, divisao ou multiplicacao
+let operacao = "divisao"; // ALTERAR PARA: soma, subtracao, divisao ou multiplicacao
 let conta = 0; // variável para o resultado de cada operação
 let valor = 1; // entrada que vai de 1 a 10, com incremento no final do DO
 
 do {
   console.log("Tabuada de " + operacao + " do " + valor);
-  for (let index = 1; index < 11; index++) {
+  for (let index = 1; index <= 10; index++) {
     switch (operacao) {
       case "soma":
         conta = valor + index;
@@ -27,4 +27,4 @@ do {
   }
   console.log("");
   valor++;
-} while (valor < 11);
+} while (valor <= 10);
